554d60d9d372193a1afb9f91a0342d62d4dda130
Ogein Imoukhuede <o.imoukhuede@aol.com>
2019-08-11 18:31:06 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,22 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  return array.each_with_index {|x, i| puts "#{i+1}. #{x}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array = array.collect {|x| "#{x.capitalize}!"}
+  return array
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  return array.any? {|x| x.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  common = array & cheese_types
+  
+  return common.first
+  
 end
