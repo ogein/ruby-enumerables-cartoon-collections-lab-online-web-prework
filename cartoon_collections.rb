3c90d46cc7a22413444a83bd2e3e32b3923ce866
Ogein Imoukhuede <o.imoukhuede@aol.com>
2019-08-11 18:19:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,7 @@ end
 
 def summon_captain_planet(array)
   array = array.map {|x| "#{x.capitalize}!"}
-  return array.join(" ")
+  return array
 end
 
 def long_planeteer_calls# code an argument here
