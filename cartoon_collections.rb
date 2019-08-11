fa6dbbd9a62fe682245a30735636dbda45e34d1f
Ogein Imoukhuede <o.imoukhuede@aol.com>
2019-08-11 18:10:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(array)
-  return array.each_with_index {|x, i| puts "#{i}. #{x}"}
+  return array.each_with_index {|x, i| puts "#{i+1}. #{x}"}
 end
 
 def summon_captain_planet# code an argument here
